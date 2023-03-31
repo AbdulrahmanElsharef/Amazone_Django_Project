6c5396a274bdff408043dfd932fa37fae0cdb0eb
unknown <abdoelsharef1@gmail.com>
2023-04-01 01:59:24 +0200

    create add review def in template

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from . views import ProductList,ProductDetail
+from . views import ProductList,ProductDetail,add_review
 
 app_name='product'
 
 urlpatterns = [
     path('products',ProductList.as_view(),name='Product_List'),
     path('products/<slug:slug>',ProductDetail.as_view(),name='Product_Detail'),
+    path("products/<slug:slug>/add_review", add_review, name="add_review")
 ]

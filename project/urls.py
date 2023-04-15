29eb0d6b9143c3eefc5aeef34453ad2ec7fffe3f
unknown <abdoelsharef1@gmail.com>
2023-04-15 16:48:45 +0200

    add django_debgger_toolbar

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls',namespace='products')),
     path('', include('order.urls',namespace='orders')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
